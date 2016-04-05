d2994a172cb085928cb453fec0f31fb05c29c7a0
Mohammad Nasir <c3mohamn@cdf.toronto.edu>
2016-04-05 16:48:13 -0400

    adding an empty search.js file to router.

---
+++ routes/search.js
@@ -0,0 +1,18 @@
+/*
+
+var express = require("express");
+var router = express.Router();
+var UserCredentials = require("../lib/UserCredentials"),
+    UserProfile = require("../lib/UserProfile");
+
+    
+    
+    TODO:
+    -- display the search page.
+    -- allow searching for mentors using username | actual name and possibily specialist.
+    -- display the search results in a table
+	  
+
+module.exports = router;
+
+*/
