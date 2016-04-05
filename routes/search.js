2dcd1224a10139607ebd384509c888c34adf03f5
Mohammad Nasir <c3mohamn@cdf.toronto.edu>
2016-04-05 19:24:16 -0400

    Updating Search

--- routes/search.js
+++ routes/search.js
@@ -1,18 +1,17 @@
-/*
-
 var express = require("express");
 var router = express.Router();
 var UserCredentials = require("../lib/UserCredentials"),
     UserProfile = require("../lib/UserProfile");
 
+// Getting the search page
+router.get('/', function(req, res, next) {
+  res.render('search', { title: 'Search' });
+});
     
-    
-    TODO:
+    /*TODO:
     -- display the search page.
     -- allow searching for mentors using username | actual name and possibily specialist.
     -- display the search results in a table
-	  
+    */
 
 module.exports = router;
-
-*/
