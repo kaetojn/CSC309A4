b7b59dfe901c919879f20597bfec8045cdab8a71
Berta <Berta>
2016-04-06 12:37:49 -0400

    Some updates, added temp admin page.

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/test', function(err) {
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var search = require('./routes/search');
+var admin = require('./routes/admin');
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/search', search);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET search page. */
+/* GET admin page. */
 router.get('/', function(req, res, next) {
-    res.render('search', { title: 'Search Tutors' });
+    res.render('admin', { title: 'admin page' });
 });
 
 module.exports = router;
