f7fd3da9420cc4e16e0cb077b3ff2d8e609c197f
Berta <Berta>
2016-04-06 11:43:41 -0400

    Updating Files to add in search bar/page.

--- routes/search.js
+++ routes/search.js
@@ -1,24 +1,9 @@
-/* TEMP FILE, WILL BE DELETED ONCE ADDED TO INDEX. */
+var express = require('express');
+var router = express.Router();
 
-// TODO: Add a search bar on the profile page
-
-// Search starts when a user enters something into a search bar.
-
-//POST to search
-router.post('/search', function(req, res){    
-    var search = req.body.search;
-    
-    // find the user that matches search
-    // Atm will only be searching using username
-    UserProfile.findOne({username: search}, function(err, user){
-       if(err){
-           console.log(err);
-           return res.status(500).send();
-       }
-       if(!user){
-           return res.status(404).send();
-       }
-    });
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.render('search', { title: 'Search Tutors' });
 });
 
-// Will redirect user to a new page, where mentors that match their results will be posted.
+module.exports = router;
